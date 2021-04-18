d1c9026a3ff2042bc6568afee70a0193a8b9a041
skeerti <srinagesh_keerti@intuit.com>
2021-04-18 15:57:36 +0530

    Mades changes to the MemeGenerator file

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <div>
+            <h1>Meme Generator</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
